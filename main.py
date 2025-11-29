0b1442c03361dd07761288d8fff0b343b60a6151
inspire12 <ox4443@naver.com>
2025-11-29 22:51:39 +0900

    feat: connection_pool 추가 개발 및 create, read 적용

--- main.py
+++ main.py
@@ -1,73 +1,61 @@
 import pymysql
 from pymysql import cursors
 
-def connection():
-    conn = pymysql.connect(
-        host="localhost",
-        port=3306,
-        user="tester",
-        password="tester",
-        database="llmagent",
-        charset="utf8mb4",
-        cursorclass=pymysql.cursors.DictCursor,
-    )
+from connection_pool import PymysqlConnectionPool
 
+pool = PymysqlConnectionPool(
+  maxsize=5,
+  host="localhost",
+  port=3306,
+  user="root",
+  password="password",
+  database="llmagent",
+)
+
+
+def connection():
+    conn = pool.get_conn()
     try:
         with conn.cursor() as cursor:
             cursor.execute("SELECT 1")
             row = cursor.fetchone()
             print(row)
     finally:
-        conn.close()
+        pool.release_connection(conn)
 
 
 def create_user(name: str, email: str):
-    conn = pymysql.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        password="password",
-        database="llmagent",
-        charset="utf8mb4",
-        cursorclass=pymysql.cursors.DictCursor,
-    )
+    conn = pool.get_conn()
     try:
         with conn.cursor() as cursor:
             sql = """
                   INSERT INTO users (name, email)
-                  VALUES (%s, %s) \
+                  VALUES (%s, %s)
                   """
             cursor.execute(sql, (name, email))
         conn.commit()
     finally:
-        conn.close()
+        pool.release_connection(conn)
 
 
 def get_user_by_email(email: str):
-    conn = pymysql.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        password="password",
-        database="llmagent",
-        charset="utf8mb4",
-        cursorclass=pymysql.cursors.DictCursor,
-    )
+    conn = pool.get_conn()
     try:
         with conn.cursor() as cursor:
             sql = """
                   SELECT id, name, email, created_at
                   FROM users
-                  WHERE email = %s \
+                  WHERE email = %s
                   """
             cursor.execute(sql, (email,))
             return cursor.fetchone()
     finally:
-        conn.close()
+        pool.release_connection(conn)
 
 
 if __name__ == '__main__':
     # connection()
     # create_user('hak', 'ox4443@naver.com')
-    print(get_user_by_email('ox4443@naver.com'))
+    for i in range(100):
+        print(get_user_by_email('ox4443@naver.com'))
 
