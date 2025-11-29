fcb0268aa119b1e2b7a5e74be0e1e50f7caf5b38
inspire12 <ox4443@naver.com>
2025-11-29 22:46:54 +0900

    feat: create, read 코드 추가

--- main.py
+++ main.py
@@ -1,16 +1,73 @@
-# This is a sample Python script.
+import pymysql
+from pymysql import cursors
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def connection():
+    conn = pymysql.connect(
+        host="localhost",
+        port=3306,
+        user="tester",
+        password="tester",
+        database="llmagent",
+        charset="utf8mb4",
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT 1")
+            row = cursor.fetchone()
+            print(row)
+    finally:
+        conn.close()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def create_user(name: str, email: str):
+    conn = pymysql.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="password",
+        database="llmagent",
+        charset="utf8mb4",
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+    try:
+        with conn.cursor() as cursor:
+            sql = """
+                  INSERT INTO users (name, email)
+                  VALUES (%s, %s) \
+                  """
+            cursor.execute(sql, (name, email))
+        conn.commit()
+    finally:
+        conn.close()
 
 
-# Press the green button in the gutter to run the script.
+def get_user_by_email(email: str):
+    conn = pymysql.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="password",
+        database="llmagent",
+        charset="utf8mb4",
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+    try:
+        with conn.cursor() as cursor:
+            sql = """
+                  SELECT id, name, email, created_at
+                  FROM users
+                  WHERE email = %s \
+                  """
+            cursor.execute(sql, (email,))
+            return cursor.fetchone()
+    finally:
+        conn.close()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # connection()
+    # create_user('hak', 'ox4443@naver.com')
+    print(get_user_by_email('ox4443@naver.com'))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
